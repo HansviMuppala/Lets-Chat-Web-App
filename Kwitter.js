83956ae37f6aec02afeb831625aa0b24fd3f95e4
Hansvi <123161387+HansviMuppala@users.noreply.github.com>
2023-08-01 19:19:23 +1000

    Add files via upload

--- Kwitter.js
+++ Kwitter.js
@@ -1,3 +1,4 @@
 function addUser(){
     localStorage.setItem("user_name", user_name);
+    window.location.replace("Kwitter_room.html")
 }
---
+++ Kwitter_room.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCFnMsgGOOjL84EI4ddjcn8XXzVu3SJYLs",
+  authDomain: "lets-chat-web-app-1dc3a.firebaseapp.com",
+  projectId: "lets-chat-web-app-1dc3a",
+  storageBucket: "lets-chat-web-app-1dc3a.appspot.com",
+  messagingSenderId: "1028616689086",
+  appId: "1:1028616689086:web:d8561801645122cc1395ef"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
